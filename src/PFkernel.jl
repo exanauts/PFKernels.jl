7ffdf6265f357552ebb5c555cf1a6f4bb65efef9
Michel Schanen <Michel.Schanen@gmail.com>
2020-10-19 12:21:52 -0500

    CUDA version

--- src/PFkernel.jl
+++ src/PFkernel.jl
@@ -12,6 +12,8 @@ module PFkernel
     include("PowerSystem/PowerSystem.jl")
     using .PowerSystem
 
+    using CUDA
+
     const PS = PowerSystem
     
     function residualFunction(V, Ybus, Sbus, pv, pq)
@@ -58,12 +60,47 @@ module PFkernel
         end
     end
 
+    function residual_kernel_cuda!(F, v_m, v_a,
+                                  ybus_re_nzval, ybus_re_colptr, ybus_re_rowval,
+                                  ybus_im_nzval, ybus_im_colptr, ybus_im_rowval,
+                                  pinj, qinj, pv, pq, nbus)
+        npv = length(pv)
+        npq = length(pq)
+        n = npq + npv
+        index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+        stride = blockDim().x * gridDim().x
+        for i in index:stride:n
+            # REAL PV: 1:npv
+            # REAL PQ: (npv+1:npv+npq)
+            # IMAG PQ: (npv+npq+1:npv+2npq)
+            fr = (i <= npv) ? pv[i] : pq[i - npv]
+            F[i] -= pinj[fr]
+            if i > npv
+                F[i + npq] -= qinj[fr]
+            end
+            @inbounds for c in ybus_re_colptr[fr]:ybus_re_colptr[fr+1]-1
+                to = ybus_re_rowval[c]
+                aij = v_a[fr] - v_a[to]
+                # f_re = a * cos + b * sin
+                # f_im = a * sin - b * cos
+                coef_cos = v_m[fr]*v_m[to]*ybus_re_nzval[c]
+                coef_sin = v_m[fr]*v_m[to]*ybus_im_nzval[c]
+                cos_val = CUDA.cos(aij)
+                sin_val = CUDA.sin(aij)
+                F[i] += coef_cos * cos_val + coef_sin * sin_val
+                if i > npv
+                    F[npq + i] += coef_cos * sin_val - coef_sin * cos_val
+                end
+            end
+        end
+    end
+
     function residualFunction_polar!(F, v_m, v_a,
                                     ybus_re, ybus_im,
                                     pinj, qinj, pv, pq, nbus)
         npv = length(pv)
         npq = length(pq)
-        residual_kernel!(F, v_m, v_a,
+        @cuda residual_kernel_cuda!(F, v_m, v_a,
                     ybus_re.nzval, ybus_re.colptr, ybus_re.rowval,
                     ybus_im.nzval, ybus_im.colptr, ybus_im.rowval,
                     pinj, qinj, pv, pq, nbus)
