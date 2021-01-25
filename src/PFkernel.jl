c5e0c65cbb20ae48043afb9f47f982d2c82adbad
Michel Schanen <mschanen@anl.gov>
2021-01-25 11:39:20 -0600

    adding roc identity kernel

--- src/PFkernel.jl
+++ src/PFkernel.jl
@@ -325,6 +325,15 @@ module PFkernel
         F .= ForwardDiff.value.(dF)
     end
 
+    function kernel_roc!(F, x) 
+        n = length(x)
+        index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+        stride = blockDim().x * gridDim().x
+        for i in index:stride:n
+            F[i] = x[i] 
+        end
+    end
+
     function kernel_cuda!(F, x) 
         n = length(x)
         index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
